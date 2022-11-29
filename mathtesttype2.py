ea1ed4a32a349cbb01ad2cc5bddf0937e6fd4d87
Sam P <119388516+InfiniteRacer@users.noreply.github.com>
2022-11-29 20:23:14 +0400

    Add files via upload

---
+++ mathtesttype2.py
@@ -0,0 +1,166 @@
+# Q1 is 8000+2000x2
+a1 = '12000'
+# Q2 is 8000+2000/2
+a2 = '9000'
+# Q3 is 8000+4000x2
+a3 = '16000'
+# Q4 is 8000+4000/2
+a4 = '10000'
+# Q5 is 8000+6000x2
+a5 = '20000'
+# Q6 is 8000+6000/2
+a6 = '11000'
+# Q7 is 8000+8000x2
+a7 = '24000'
+# Q8 is 8000+8000/2
+a8 = '12000'
+# Q9 is 8000+10000x2
+a9 = '28000'
+# Q10 is 8000+10000/2
+a10 = '13000'
+# Q11 is 8000+12000x2
+a11 = '32000'
+# Q12 is 8000+12000/2
+a12 = '14000'
+# Q13 is 8000+14000x2
+a13 = '36000'
+# Q14 is 8000+14000/2
+a14 = '15000'
+# Q15 is 8000+16000/2
+a15 = '16000'
+# Q16 is 8000+16000x2
+a16 = '40000'
+# Q17 is 8000+18000/2
+a17 = '17000'
+# Q18 is 8000+18000x2
+a18 = '44000'
+# Q19 is 8000+20000/2
+a19 = '18000'
+# Q20 is 8000+20000x2
+a20 = '48000'
+
+print("Hey! Welcome to the quiz.")
+name=input("Whats your name? ")
+print("Nice to meet you " +name+ ".")
+print("Theres only one rule:")
+
+print("One question wrong, and you have to restart")
+q1=input("What is 8000+2000x2? ")
+
+if q1 == a1:
+    print("Correct! Next question.")
+    q2=input("What is 8000+2000/2? ")
+    
+    if q2 == a2:
+        print("Correct! Next question.")
+        q3=input("What is 8000+4000x2? ")
+        
+        if q3 == a3:
+            print("Correct! Next question.")
+            q4=input("What is 8000+4000/2? ")
+            
+            if q4 == a4:
+                print("Correct! Next question.")
+                q5=input("What is 8000+6000x2? ")
+                
+                if q5 == a5:
+                    print("Correct! Next question.")
+                    q6=input("What is 8000+6000/2? ")
+                    
+                    if q6 == a6:
+                        print("Correct! Next question.")
+                        q7=input("What is 8000+8000x2? ")
+                        
+                        if q7 == a7:
+                            print("Correct! Next question.")
+                            q8=input("What is 8000+8000/2? ")
+                            
+                            if q8 == a8:
+                                print("Correct! Next question.")
+                                q9=input("What is 8000+10000x2? ")
+                                
+                                if q9 == a9:
+                                    print("Correct! Next question.")
+                                    q10=input("What is 8000+10000/2? ")
+                                    
+                                    if q10 == a10:
+                                        print("Correct! Next question.")
+                                        q11=input("What is 8000+12000x2? ")
+                                        
+                                        if q11 == a11:
+                                            print("Correct! Next question.")
+                                            q12=input("What is 8000+12000/2? ")
+                                            
+                                            if q12 == a12:
+                                                print("Correct! Next question.")
+                                                q13=input("What is 8000+14000x2? ")
+                                                
+                                                if q13 == a13:
+                                                    print("Correct! Next question.")
+                                                    q14=input("What is 8000+14000/2? ")
+                                                    
+                                                    if q14 == a14:
+                                                        print("Correct! Next question.")
+                                                        q15=input("What is 8000+16000x2? ")
+                                                        
+                                                        if q15 == a15:
+                                                            print("Correct! Next question.")
+                                                            q16=input("What is 8000+16000/2? ")
+                                                            
+                                                            if q16 == a16:
+                                                                print("Correct! Next question.")
+                                                                q17=input("What is 8000+18000x2? ")
+                                                                
+                                                                if q17 == a17:
+                                                                    print("Correct! Next question.")
+                                                                    q18=input("What is 8000+18000/2? ")
+                                                                    
+                                                                    if q18 == a18:
+                                                                        print("Correct! Next question.")
+                                                                        q19=input("What is 8000+20000x2? ")
+                                                                        
+                                                                        if q19 == a19:
+                                                                            print("Correct! Next question. ")
+                                                                            q20=input("What is 8000+2000/2? ")
+                                                                            
+                                                                            if q20 == a20:
+                                                                                print("Correct! You won and completed the quiz.")
+                                                                                print("Well done " +name+ ".")
+                                                                            else:
+                                                                                print("Wrong... Try the quiz again! You were one off.")
+                                                                    else:
+                                                                        print("Wrong... Try the quiz again!")
+                                                                else:
+                                                                    print("Wrong... Try the quiz again!")
+                                                            else:
+                                                                print("Wrong... Try the quiz again!")
+                                                        else:
+                                                            print("Wrong... Try the quiz again!")
+                                                    else:
+                                                        print("Wrong... Try the quiz again!")
+                                                else:
+                                                    print("Wrong... Try the quiz again!")
+                                            else:
+                                                print("Wrong... Try the quiz again!")
+                                        else:
+                                            print("Wrong... Try the quiz again!")
+                                    else:
+                                        print("Wrong... Try the quiz again!")
+                                else:
+                                    print("Wrong... Try the quiz again!")
+                            else:
+                                print("Wrong... Try the quiz again!")
+                        else:
+                            print("Wrong... Try the quiz again!")
+                    else:
+                        print("Wrong... Try the quiz again!")
+                else:
+                    print("Wrong... Try the quiz again!")
+            else:
+                print("Wrong... Try the quiz again!")
+        else:
+            print("Wrong... Try the quiz again!")
+    else:
+        print("Wrong... Try the quiz again!")
+else:
+    print("Wrong... Try the quiz again!")
